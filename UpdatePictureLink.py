6f4a6c5e1de04a1b554d9391781ad1357e70a9aa
yurabysaha <yurabysaha@gmail.com>
2016-07-07 11:03:18 +0300

    Select picture logic and view

--- UpdatePictureLink.py
+++ UpdatePictureLink.py
@@ -2,6 +2,7 @@ import os
 import tkMessageBox
 import tkSimpleDialog
 import Tkinter as tk
+from tkFileDialog import askopenfilename
 
 import xml.etree.ElementTree as ET
 def xmlpath():
@@ -12,35 +13,29 @@ def xmlpath():
 
 ROOT_PATH=os.getcwd() + xmlpath()
 
+
+def open_file_handler():
+    filePath = askopenfilename()
+
+    doc = ET.parse(ROOT_PATH + 'pictureLink.xml')
+    root = doc.getroot()
+    django = doc.find('link')
+    django.text = filePath
+    doc.write(ROOT_PATH + 'pictureLink.xml', encoding="utf-8", xml_declaration=True)
+
+    tkMessageBox.showinfo(
+    "Updated",
+    "Picture is updated"
+)
+
+
 class UpdateLink:
     def __init__(self, body):
-        self.link = tk.Entry(body,
-                         font="Verdana 12",
-                          width=50)
-        self.link.grid(row = 1, column = 2)
+        self.but = tk.Button(body, command=open_file_handler, padx=100, text="Select Picture")
+        self.but.grid(row = 1, column = 2)
 
         title = tk.Label(body, text="Picture link", font="Arial 12")
         title.grid(row = 1, column = 1)
-
-        but = tk.Button(body,
-                            text="Update",
-                            width=6, height=1,
-                            bg="red", fg="black")
-
-        but.bind("<Button-1>", self.updatePLink)
-        but.grid(row = 1, column = 3)
-
-    def updatePLink(self, event):
-        doc = ET.parse(ROOT_PATH + 'pictureLink.xml')
-        root = doc.getroot()
-        django = doc.find('link')
-        django.text = self.link.get()
-
-        doc.write(ROOT_PATH + 'pictureLink.xml', encoding="utf-8", xml_declaration=True)
-        tkMessageBox.showinfo(
-            "Updated",
-            "Picture link is updated"
-        )
 
 
 
